6529f12e4afcfa7ced79b3ac1b619fa605016e4b
Lian Thompson <lianthompson@Lians-MacBook-Pro-2.local>
2018-08-10 09:41:36 -0700

    remove default styling

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -8,8 +8,6 @@ const IndexPage = () => (
       <Jumbotron>
         <h1 className="display-3">Hi, we're Colark.</h1>
         <p className="lead">We start, buy, and invest in wonderful internet businesses.</p>
-        <hr className="my-2" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
         <div id="container">
         <div>
           <h3>Phase Zero</h3>
