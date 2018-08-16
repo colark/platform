b08332215e76fb91aa06011d799d3a5671f803e2
Lian Thompson <lianthompson@Lians-MacBook-Pro-2.local>
2018-08-16 09:43:52 -0700

    delete dead files

--- gatsby-site/src/components/Navbar/colarklogo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import logo from "./logo.png"; // Tell Webpack this JS file uses this image
-
-function Logo(){
-  return <img src={logo} alt="Logo" />;
-}
-
-export default Logo;
