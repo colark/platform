a584d26ecea56dea0aa2a97ea3f9743928f0451d
Lian Thompson <lianthompson@Lians-MacBook-Pro-2.local>
2018-08-16 14:45:59 -0700

    move projects to separate component and add shadow

--- gatsby-site/src/components/View/index.js
+++ gatsby-site/src/components/View/index.js
@@ -4,6 +4,7 @@ import Link from 'gatsby-link'
 import './View.css';
 import Illustration from './Background/Illustration.js';
 import ReactNavbar from '../Navbar/index.js';
+import Projects from '../Projects/index.js';
 
 const View = (props) => {
   return (
@@ -15,15 +16,8 @@ const View = (props) => {
           <h1 className="display-3">Building <br></br> better software, together.</h1>
         </div>
         <p className="lead">Teams that consistently perform at the highest levels are able to come together and be unified across the organization - staff, players, coaches, management, and ownership. When everyone is on the same page, trust develops and teams can grow and succeed together.</p>
-        <div className="project-container">
-          <div className="project">
-          <img src='http://res.cloudinary.com/colark/image/upload/r_11/v1534365918/Colark%20Marketing%20Site/PhaseZero.png'/>
-          </div>
-          <div className="project">
-          <img src='http://res.cloudinary.com/colark/image/upload/r_10/v1534365717/Colark%20Marketing%20Site/Unstack.png'/>
-          </div>
-        </div>
       </Jumbotron>
+      <Projects />
     </div>
   );
 };
