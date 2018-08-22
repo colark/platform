3cc44af04b3b103b5c18a9fd548b67d1fa90e26b
alodahl <alodahl@pointloma.edu>
2018-08-22 11:47:37 -0700

    initial commit for partner components

--- gatsby-site/src/components/partner/partner.js
+++ gatsby-site/src/components/partner/partner.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
-// import Img from 'gatsby-image'
+import Illustration2 from './illustration2/Illustration2'
 import styles from "./partner.css"
 
-export default function TeamMember(props) {
+export default function Partner(props) {
   return(
-    <div className="partner">
-      <div className="partner__logo-div">
-        <img src={props.data.logo} alt={props.data.name} className="partner__photo" />
+    <div className="partner-container">
+      <Illustration2/>
+      <div className="partner">
+        <div className="partner__logo-div">
+          <img src={props.logo} alt={props.name} className="partner__photo" />
+        </div>
+        <div className="partner__info">
+          <Link className="partner__website" to={props.website} target="__blank">Website</Link>
+        </div>
       </div>
-      <div className="partner__info">
-        <h3 className="partner__name">{props.data.name}</h3>
-        <Link className="partner__website" to={props.data.website} target="__blank">Website</Link>
-      </div>
-  </div>
+    </div>
   );
 }
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -3,10 +3,12 @@ import { Jumbotron, Button } from 'reactstrap';
 import Link from 'gatsby-link'
 import Team from '../components/team/team'
 import View from '../components/View';
+import Partner from '../components/partner/Partner';
 
 const IndexPage = () => (
   <div>
     <View/>
+    <Partner/>
     <Team/>
   </div>
 )
