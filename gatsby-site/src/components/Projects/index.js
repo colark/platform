05bef58274ac785996e437f0d61dd3eb7ed07c3d
Lian Thompson <lianthompson@lians-mbp-2.gateway.pace.com>
2018-08-23 13:43:01 -0700

    wave component

--- gatsby-site/src/components/Projects/index.js
+++ gatsby-site/src/components/Projects/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './projects.css';
+import BackgroundWave from '../BackgroundWave/index.js';
+
 
 const Projects = (props) => {
   return (
-    <div className="project-container">
-        <div className="project">
-          <img className="image" src='https://res.cloudinary.com/colark/image/upload/v1534454636/Colark%20Marketing%20Site/Colark_Marketing_Site_PhaseZeroEdited.png'/>
-        </div>
-        <div className="project">
-          <img className="image" src='http://res.cloudinary.com/colark/image/upload/r_10/v1534365717/Colark%20Marketing%20Site/Unstack.png'/>
-        </div>
+    <div>
+      <BackgroundWave/>
+      <div className="project-container">
+          <div className="project">
+            <img className="image" src='https://res.cloudinary.com/colark/image/upload/v1534454636/Colark%20Marketing%20Site/Colark_Marketing_Site_PhaseZeroEdited.png'/>
+          </div>
+          <div className="project">
+            <img className="image" src='http://res.cloudinary.com/colark/image/upload/r_10/v1534365717/Colark%20Marketing%20Site/Unstack.png'/>
+          </div>
+      </div>
     </div>
   );
 };
--- gatsby-site/src/components/Waves/index.js
+++ gatsby-site/src/components/Waves/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import PurpleWaveTop from './BackgroundWave/PurpleWave/PurpleWaveTop.js';
-import BlueWaveTop from './BackgroundWave/BlueWave/BlueWaveTop.js';
+import PurpleWaveTop from './PurpleWave/PurpleWaveTop.js';
+import BlueWaveTop from './BlueWave/BlueWaveTop.js';
+import BlueWaveBottom from './BlueWave/BlueWaveBottom.js';
+import PurpleWaveBottom from './PurpleWave/PurpleWaveBottom.js';
 
 const Waves = (props) => {
   return (
     <div>
       <PurpleWaveTop/>
       <BlueWaveTop/>
+      <BlueWaveBottom/>
+      <PurpleWaveBottom/>
     </div>
   );
 };
