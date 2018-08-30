110f5964729460d16bd1c8d89a9dc625a90f8ed6
alodahl <alodahl@pointloma.edu>
2018-08-30 09:48:33 -0700

    finish footer, add first social media urls, change team component re: footer spacing issues

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -4,12 +4,16 @@ import Link from 'gatsby-link'
 import Team from '../components/team/team'
 import View from '../components/View';
 import Projects from '../components/Projects/index.js';
+import Footer from '../components/footer/Footer.js';
 
 const IndexPage = () => (
   <div>
-    <View/>
-    <Projects />
-    <Team/>
+    <div className="body-container-minus-footer">
+      <View/>
+      <Projects />
+      <Team/>
+    </div>
+    <Footer/>
   </div>
 )
 
