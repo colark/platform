cbd09f632408aa41dc184e1aaaf96c3cd86f518d
alodahl <alodahl@pointloma.edu>
2018-09-05 20:52:19 -0700

    fix merge errors between basicpage and footer

--- gatsby-site/src/components/landing-page/LandingPage.js
+++ gatsby-site/src/components/landing-page/LandingPage.js
@@ -1,23 +1,20 @@
 import React from 'react'
+import BasicPage from '../BasicPage'
 import Team from '../team/team'
 import View from '../View';
-import ReactNavbar from '../Navbar';
 import BlueWaveBottom from '../blue-wave-bottom/BlueWaveBottom';
 import Partners from '../partners/Partners';
 import Projects from '../Projects';
-import Footer from '../footer/Footer';
 
 const LandingPage = () => (
   <div>
-    <div className="body-container-minus-footer">
-      <ReactNavbar />
+    <BasicPage>
       <View/>
-      <Projects />
+      <Projects/>
       <Partners/>
       <BlueWaveBottom/>
       <Team/>
-    </div>
-    <Footer/>
+    </BasicPage>
   </div>
 );
 
