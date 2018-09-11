cad0a451b15183d78eafc12a69d95be7427a5126
alodahl <alodahl@pointloma.edu>
2018-09-11 10:52:22 -0700

    remove unnecessary imports

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { Route, Switch } from 'react-router';
 import BasicPage from '../components/basic-page/BasicPage'
 import LandingPage from '../components/landing-page/LandingPage'
-import BlogMenu from '../components/blog-menu/BlogMenu'
-import BlogArticle from '../components/blog-article/BlogArticle'
-import ProjectInfo from '../components/ProjectInfo'
 
 const IndexPage = () => (
   <div>
