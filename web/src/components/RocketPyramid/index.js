fa965723a4772c7cef2f39c5e1c811b591668d64
Lian <thompsonlian@yahoo.com>
2018-09-24 17:11:14 -0700

    remove bootstrap, make background image its own component, re-label image CSS names

---
+++ web/src/components/RocketPyramid/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Illustration = (props) => {
+  return (
+    <div className="rocket-pyramid">
+      <img className="rocket-pyramid__image" src="https://res.cloudinary.com/colark/image/upload/v1536855207/Illustration.svg"/>
+    </div>
+  );
+};
+
+export default Illustration;
--- web/src/components/View/index.js
+++ web/src/components/View/index.js
@@ -1,16 +1,15 @@
+
 import React from 'react'
-import { Jumbotron, Button } from 'reactstrap';
-import { Link } from 'react-router';
-import Illustration from './Background/Illustration.js';
+import RocketPyramid from '../RocketPyramid';
 
 const View = (props) => {
   return (
-    <div id="view-container">
-      <Illustration/>
-      <Jumbotron>
-          <h1 className="display-3">Go from <strong>zero</strong> to <strong>product-market</strong> fit</h1>
-        <p className="lead">(with no throwaway code)</p>
-      </Jumbotron>
+    <div className="view-container">
+      <RocketPyramid/>
+      <div className="view-container--inner">
+          <h1 className="view-container__title">Go from <strong>zero</strong> to <strong>product-market</strong> fit</h1>
+          <p className="view-container__subtitle">(with no throwaway code)</p>
+      </div>
     </div>
   );
 };
