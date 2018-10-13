1f3503341ed15c823378e8424278c82a4f012188
Lian <thompsonlian@yahoo.com>
2018-10-13 11:03:10 -0700

    Partner component using GraphQL

---
+++ backend/api/data/partnerList.js
@@ -0,0 +1,29 @@
+const partnerList = [
+  {
+    name: "Constellation Labs",
+    logo: "https://res.cloudinary.com/colark/image/upload/v1534977373/Colark%20Marketing%20Site/constellation-labs.svg",
+    website: "https://constellationlabs.io/"
+  },
+  {
+    name: "Career Karma",
+    logo: "https://res.cloudinary.com/colark/image/upload/v1534977138/Colark%20Marketing%20Site/career-karma.png",
+    website: "https://careerkarma.io/"
+  },
+  {
+    name: "Atlassian",
+    logo: "https://res.cloudinary.com/colark/image/upload/v1534977138/Colark%20Marketing%20Site/Atlassian.svg",
+    website: "https://www.atlassian.com/"
+  },
+  {
+    name: "8heroes",
+    logo: "https://res.cloudinary.com/colark/image/upload/v1534977138/Colark%20Marketing%20Site/8heroes.png",
+    website: "https://www.facebook.com/The8Heroes/"
+  },
+  {
+    name: "mRelief",
+    logo: "https://res.cloudinary.com/colark/image/upload/v1534977138/Colark%20Marketing%20Site/mRelief.png",
+    website: "https://www.mrelief.com/"
+  }
+];
+
+export default partnerList;
--- backend/api/resolvers/index.js
+++ backend/api/resolvers/index.js
@@ -1,6 +1,9 @@
+import partnerList from '../data/partnerList.js';
+
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
+    partnerList: () => partnerList,
   },
 };
 
