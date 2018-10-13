a15aec3d00eb7c2d8861140a097be54ab0b22f11
Lian <thompsonlian@yahoo.com>
2018-10-13 09:22:09 -0700

    Projects Component using GraphQL

--- backend/api/resolvers/index.js
+++ backend/api/resolvers/index.js
@@ -1,11 +1,10 @@
 import projectList from "../data/projectList.js";
-// import "../photos/phase_zero.jpeg";
 
 const resolvers = {
   Query: {
-    projectList: () =>
-     projectList
-  }
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+    projectList: () => projectList,
+  },
 };
 
 export default resolvers;
