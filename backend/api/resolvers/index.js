562be1df1bc6e8becd915a2bfb236a7993911cea
Lian <thompsonlian@yahoo.com>
2018-10-15 07:07:16 -0700

    import teamMemberList in resolvers

--- backend/api/resolvers/index.js
+++ backend/api/resolvers/index.js
@@ -1,6 +1,7 @@
 import navbarList from '../data/navbarList.js';
 import partnerList from '../data/partnerList.js';
 import projectList from "../data/projectList.js";
+import teamMembersList from "../data/teamMembersList.js";
 
 const resolvers = {
   Query: {
