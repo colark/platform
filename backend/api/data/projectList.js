c05e7c73473ecc29f21d6862e7651bb10b847123
alodahl <alodahl@pointloma.edu>
2018-10-18 18:27:05 +0300

    replace icons w shadowed icons

--- backend/api/data/projectList.js
+++ backend/api/data/projectList.js
@@ -1,13 +1,13 @@
 const projectList = [
   {
     name: "Unstack",
-    icon: "https://res.cloudinary.com/colark/image/upload/v1539723991/Colark%20Marketing%20Site/unstack-icon.svg",
+    icon: "https://res.cloudinary.com/colark/image/upload/v1539876051/Colark%20Marketing%20Site/UNSTACKWSHADOW.svg",
     title: "https://res.cloudinary.com/colark/image/upload/v1539725510/Colark%20Marketing%20Site/unstack-title.svg",
     blurb: "Unstack configures your project so your team can start creating value on day 1."
   },
   {
     name: "Phase Zero",
-    icon: "https://res.cloudinary.com/colark/image/upload/v1539724222/Colark%20Marketing%20Site/phase-zero-icon.svg",
+    icon: "https://res.cloudinary.com/colark/image/upload/v1539876051/Colark%20Marketing%20Site/PHASEZEROWSHADOW.svg",
     title: "https://res.cloudinary.com/colark/image/upload/v1539725504/Colark%20Marketing%20Site/phase-zero-title.svg",
     blurb: "Phase Zero supports teams using Unstack with access to a community of experts."
   }
