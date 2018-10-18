fc3baf36caa06fd68306a796cf3c00336c282b14
Lian <thompsonlian@yahoo.com>
2018-10-18 08:20:12 -0700

    update hero sentence and spacing

--- web/src/components/View/index.js
+++ web/src/components/View/index.js
@@ -7,8 +7,7 @@ const View = (props) => {
     <div className="view-container">
       <RocketPyramid/>
       <div className="view-container--inner">
-          <h1 className="view-container__title">Go from <strong>zero</strong> to <strong>product-market</strong> fit</h1>
-          <p className="view-container__subtitle">(with no throwaway code)</p>
+          <h1 className="view-container__title">Build sustainable software at any scale</h1>
       </div>
     </div>
   );
