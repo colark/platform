33e7dc982dff658080a238ca982b585a73fb4b5a
Lian <thompsonlian@yahoo.com>
2018-10-21 05:13:56 -0700

    maze-background

--- web/src/components/View/index.js
+++ web/src/components/View/index.js
@@ -5,7 +5,8 @@ const View = (props) => {
   return (
     <div className="view-container">
       <div className="view-container--inner">
-          <h1 className="view-container__title">Build sustainable software at any scale</h1>
+        <h1 className="view-container__title">Build sustainable software at any scale</h1>
+        <img className="view-container__background--maze" src="https://res.cloudinary.com/colark/image/upload/v1539949738/Colark%20Marketing%20Site/Group_6.svg"/>
       </div>
     </div>
   );
