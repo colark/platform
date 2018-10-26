089985a73f4818381bbbfa6ff93bd26c81bfe3d2
Lian <thompsonlian@yahoo.com>
2018-10-26 16:23:50 -0700

    add mail icon

--- web/src/components/footer/Footer.js
+++ web/src/components/footer/Footer.js
@@ -3,6 +3,11 @@ import React from 'react'
 export default function Footer(props) {
     const footerIcons = [
     {
+      imageUrl: 'https://res.cloudinary.com/colark/image/upload/v1540595865/Colark%20Marketing%20Site/email2.svg.svg',
+      linkUrl: 'mailto:ellie@colark.com',
+      alt: 'Email Colark'
+    },
+    {
       imageUrl: 'https://res.cloudinary.com/colark/image/upload/v1540595601/Colark%20Marketing%20Site/LINKEDIN.svg',
       linkUrl: 'https://www.linkedin.com/company/colark/',
       alt: 'Find Colark on LinkedIn'
