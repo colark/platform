6b94fc508d6cd6d15be9729c35363be62ff338f6
Lian <thompsonlian@yahoo.com>
2018-10-26 14:37:29 -0700

    make white background component and add bottom maze

---
+++ web/src/components/white-overlay/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function WhiteBackground(props) {
+
+  return(
+      <div>
+        <img className="view-container__background--white" src="https://res.cloudinary.com/colark/image/upload/v1540497326/Colark%20Marketing%20Site/Vector_2_1.png"/>
+      </div>
+  );
+}
