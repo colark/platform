971804a1119085c2a44ae626afa636cbd8ade9be
Lian <thompsonlian@yahoo.com>
2018-11-07 21:59:07 -0800

    responsive large screen

--- web/src/components/HeroPage/index.js
+++ web/src/components/HeroPage/index.js
@@ -11,17 +11,10 @@ class HeroPage extends React.Component {
     return (
       <div>
         <div className="hero-container">
-        <h1 className="hero-container__title">Build sustainable software at any scale</h1>
-
-        <img className="hero-container__background--maze" src="https://res.cloudinary.com/colark/image/upload/v1539949738/Colark%20Marketing%20Site/Group_6.svg"/>
-        <div className="hero-container--inner">
-
-
-              {/* <div className="hero-container--inner"> */}
-              {/* <WhiteBackground /> */}
-              {/* </div> */}
-          {/* {this.props.children} */}
-        </div>
+          <div className="hero-container--inner">
+          <h1 className="hero-container__title">Build sustainable software at any scale</h1>
+          </div>
+          <img className="hero-container__background--maze" src="https://res.cloudinary.com/colark/image/upload/v1539949738/Colark%20Marketing%20Site/Group_6.svg" />
         </div>
       </div>
     );
--- web/src/components/MazeBottom/index.js
+++ web/src/components/MazeBottom/index.js
@@ -4,7 +4,7 @@ export default function MazeBottom(props) {
 
   return(
       <div className="body-lower-half">
-      <img src="https://res.cloudinary.com/colark/image/upload/v1540495663/Colark%20Marketing%20Site/downillustration.svg" />
+      <img className="body-lower-half--image" src="https://res.cloudinary.com/colark/image/upload/v1540495663/Colark%20Marketing%20Site/downillustration.svg" />
       </div>
   );
 }
