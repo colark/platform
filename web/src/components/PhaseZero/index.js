c78e20d4cd9e0522086ff581a30923ffe5475ebe
Lian <thompsonlian@yahoo.com>
2018-11-10 21:35:59 -0800

    create project files and routes

---
+++ web/src/components/PhaseZero/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import BasicPage from '../BasicPage'
+
+const PhaseZero = () => (
+  <div>
+    <BasicPage>
+      <div className="phasezero-maze--top">
+        <img src="https://res.cloudinary.com/colark/image/upload/v1540592123/Colark%20Marketing%20Site/Phasezerotopillustration.svg"/>
+      </div>
+      <div className="phasezero-maze--bottom">
+        <img src="https://res.cloudinary.com/colark/image/upload/v1540592165/Colark%20Marketing%20Site/Phazezerodownillustration.svg"/>
+      </div>
+    </BasicPage>
+  </div>
+);
+
+export default PhaseZero;
---
+++ web/src/components/Unstack/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import BasicPage from '../BasicPage'
+
+const Unstack = () => (
+  <div>
+    <BasicPage>
+      <div className="unstack-maze--top">
+        <img src="https://res.cloudinary.com/colark/image/upload/v1540592197/Colark%20Marketing%20Site/unstacktopillustration.svg"/>
+      </div>
+      <div className="unstack-maze--bottom">
+        <img src="https://res.cloudinary.com/colark/image/upload/v1540592301/Colark%20Marketing%20Site/Unstacdownillustration.svg"/>
+      </div>
+    </BasicPage>
+  </div>
+);
+
+export default Unstack
